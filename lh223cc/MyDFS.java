d7846be87e7dbe52858b04219cb27026e193a6a0
Linus <linushvenfelt@gmail.com>
2021-10-21 18:06:56 +0200

    passing all dfs tests

--- lh223cc/MyDFS.java
+++ lh223cc/MyDFS.java
@@ -1,5 +1,6 @@
 package lh223cc;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -15,6 +16,7 @@ public class MyDFS<E> implements DFS<E> {
     private List<Node<E>> list = new LinkedList<Node<E>>();
     private Set<Node<E>> visited = new HashSet<Node<E>>();
     private int depth = 0;
+    private int test = 0;
 
 
     @Override
@@ -72,36 +74,89 @@ public class MyDFS<E> implements DFS<E> {
         return list;
     }
 
+    private void innerPostOrder(Node<E> node) {
+
+        visited.add(node);
+        
+        Iterator<Node<E>> successors = node.succsOf();
+
+        while (successors.hasNext()) {
+            Node<E> succ = successors.next();
+
+            if (!visited.contains(succ)) {                
+                innerPostOrder(succ);
+            }
+        }
+        
+        node.num = test++;
+        list.add(node);
+        
+    }
+
     @Override
     public List<Node<E>> postOrder(DirectedGraph<E> g, Node<E> root) {
-        // TODO Auto-generated method stub
-        return null;
+        clear();
+        innerPostOrder(root);
+        return list;
     }
 
     @Override
     public List<Node<E>> postOrder(DirectedGraph<E> g) {
-        // TODO Auto-generated method stub
-        return null;
+        clear();
+        Iterator<Node<E>> iterator = g.iterator();
+
+        while (iterator.hasNext()) {
+            Node<E> node = iterator.next();
+
+            if (!visited.contains(node)) {
+                innerPostOrder(node);
+            }
+
+        }
+
+        return list;
     }
 
     @Override
     public List<Node<E>> postOrder(DirectedGraph<E> g, boolean attach_dfs_number) {
-        clear();
         
-        
-        return null;
+        Iterator<Node<E>> iterator = g.iterator();
+
+        while (iterator.hasNext()) {
+            Node<E> node = iterator.next();
+
+            if (attach_dfs_number) {
+                node.num = depth++;
+            }
+            innerPostOrder(node);
+        }
+
+        return list;
     }
 
     @Override
     public boolean isCyclic(DirectedGraph<E> graph) {
-        // TODO Auto-generated method stub
+
+        Iterator<Node<E>> iterator = postOrder(graph).iterator();
+        while (iterator.hasNext()) {
+            Node<E> nextnode = iterator.next();
+
+            Iterator<Node<E>> successors = nextnode.succsOf();
+            while (successors.hasNext()) {
+                if (nextnode.num <= successors.next().num) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
     @Override
     public List<Node<E>> topSort(DirectedGraph<E> graph) {
-        // TODO Auto-generated method stub
-        return null;
+        List<Node<E>> list = postOrder(graph);
+        Collections.reverse(list);
+        return list;
     }
     
 }
