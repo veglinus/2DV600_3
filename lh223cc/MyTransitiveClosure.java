fb51bbab321a99e31b5e170e76e21710b4cc2478
Linus <linushvenfelt@gmail.com>
2021-10-22 14:15:21 +0200

    Transitive Closure passing tests

--- lh223cc/MyTransitiveClosure.java
+++ lh223cc/MyTransitiveClosure.java
@@ -1,17 +1,35 @@
 package lh223cc;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 
 import graphs.DirectedGraph;
 import graphs.Node;
 import graphs.TransitiveClosure;
+import graphs.DFS;
 
 public class MyTransitiveClosure<E> implements TransitiveClosure<E> {
 
+    private Map< Node<E>, Collection<Node<E>> > map = new HashMap<>();
+    private Collection<Node<E>> nodes = new HashSet<Node<E>>();
+
     @Override
     public Map<Node<E>, Collection<Node<E>>> computeClosure(DirectedGraph<E> dg) {
-        // TODO Auto-generated method stub
-        return null;
+        map.clear();
+        nodes.clear();
+
+        Iterator <Node<E>> iterator = dg.iterator();
+
+        while (iterator.hasNext()) {
+            Node<E> current = iterator.next();
+            DFS<E> myDFS = new MyDFS<E>();
+
+            nodes = myDFS.dfs(dg, current);
+            map.put(current, nodes);
+        }
+
+        return map;
     }
-    
 }
